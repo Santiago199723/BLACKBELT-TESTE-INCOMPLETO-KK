5b59fb4f3ef8d242e42a2bab732600e9c08fc5c1
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2024-03-19 20:20:46 -0300

    teste

---
+++ signin.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let btn = document.querySelector('.fa-eye');
+  let inputSenha = document.querySelector('#senha');
+  let msgError = document.querySelector('#msgError');
+
+  btn.addEventListener('click', () => {
+    if (inputSenha.getAttribute('type') === 'password') {
+      inputSenha.setAttribute('type', 'text');
+    } else {
+      inputSenha.setAttribute('type', 'password');
+    }
+  });
+
+  function showErrorMessage(message) {
+    msgError.style.display = 'block';
+    msgError.innerHTML = message;
+  }
+
+  function hideErrorMessage() {
+    msgError.style.display = 'none';
+    msgError.innerHTML = '';
+  }
+
+  function entrar() {
+    console.log("Tentativa de login iniciada.");
+    hideErrorMessage();
+    showLoading();
+
+    const senha = document.getElementById('senha').value.trim(); // Obter o valor da senha
+
+    // Autenticar com Firebase usando a senha fornecida e um email fictício
+    firebase.auth().signInWithEmailAndPassword('admin@gmail.com','admin8@gmail.com', senha)
+      .then(() => {
+        console.log("Login bem-sucedido.");
+        hideLoading();
+        window.location.href = "indicador3.html"; // Redirecionar para a outra página após o login bem-sucedido
+      })
+      .catch((error) => {
+        console.error("Erro durante o login:", error);
+        hideLoading();
+        showErrorMessage('Senha incorreta. Verifique e tente novamente.');
+      });
+  }
+
+  const btnEntrar = document.querySelector('#btnEntrar');
+  if (btnEntrar) {
+    btnEntrar.addEventListener('click', entrar);
+  }
+});
